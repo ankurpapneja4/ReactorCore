30d7b85baf391a131967d26b9e92d20f7cfa5162
Ankur Papneja <ankurpapneja4@gmail.com>
2024-02-16 16:51:46 +0530

    Error Handling Demo

--- Core/src/test/java/example/reactor/core/MonoTest.java
+++ Core/src/test/java/example/reactor/core/MonoTest.java
@@ -10,7 +10,7 @@ public class MonoTest {
     BookRepository bookRepository = new BookRepository();
 
     @Test
-    void monoPublisherSubscriberTest() {
+    void monoPublisherSubscriberExample() {
 
         // Publisher
         Mono<Book> publisher = Mono.fromSupplier( bookRepository::findOne );
@@ -32,8 +32,21 @@ public class MonoTest {
         publisher.subscribe( this::printSquare );
 
     }
-
     public void printSquare(int i){ System.out.println( i*i ); }
 
+    @Test
+    void monoSubscriberExample() {
+
+        Consumer<Integer>   onNext     = i -> System.out.println("Inside onNext " + i);
+        Consumer<Throwable> onError    = exp -> System.out.println( "Inside onError: " + exp.getMessage() );
+        Runnable            onComplete = () -> System.out.println("Inside onComplete");
+
+        // Publisher
+        Mono<Integer> publisher = Mono.just( 13 );
+
+        // Subscriber
+        publisher.subscribe( onNext, onError, onComplete);
+
+    }
 
 }
